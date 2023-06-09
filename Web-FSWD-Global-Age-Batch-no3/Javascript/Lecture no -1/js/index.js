2390c47b095b7797b1247ca741a918e67cedb1e3
fazi1live <faziislive@gmail.com>
2023-06-09 20:00:13 +0500

    add

---
+++ Web-FSWD-Global-Age-Batch-no3/Javascript/Lecture no -1/js/index.js
@@ -0,0 +1,29 @@
+// block vs global
+// console.log(y)
+// let x = 10;
+
+
+//     let y = 15;
+
+
+//Difference between let and const
+
+// let z = 10;
+// let z = "hi"
+
+let myName = "john"
+myName = "jane"
+
+console.log(myName)
+
+
+
+
+// objects
+
+let myProperties = {
+    name: "John",
+    age: 30
+}
+
+
---
+++ Web-FSWD-Global-Age-Batch-no3/Javascript/Lecture no -1/readme.txt
@@ -0,0 +1,6 @@
+Today's Lecture
+
+1 Scope of variable
+2 complex or derived Data types
+3 Funtions
+4 flow control and loops
