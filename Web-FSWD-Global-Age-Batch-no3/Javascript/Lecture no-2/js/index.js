2ac1e94f9354b7f07b4fa0de8d5ad91f2a208775
fazi1live <faziislive@gmail.com>
2023-06-11 19:55:06 +0500

    addd

---
+++ Web-FSWD-Global-Age-Batch-no3/Javascript/Lecture no-2/js/index.js
@@ -0,0 +1,46 @@
+// Complex or dervied Data types
+
+
+// An Object is a collection of key-value pairs and it is user defined data
+
+// let Human  = {
+//     name: 'John',
+//     age: 30,
+//     gender: 'male'
+// }
+
+
+let nameArray = ['asdas','asdasda','adsasdasd','iugdiuygasd','oiudiuashd']
+let human1Name = undefined
+let human1Age = "asdasd"
+let human1Gender = "asdasd"
+
+
+let Hamad  = {
+    name: 'Hamad',
+    age: 30,
+    gender: 'male'
+}
+
+
+let Alia  = {
+    name: 'Alia',
+    age: 5,
+    gender: 'female',
+    Muslim:true,
+    identityMark:null,
+    occupation:undefined,
+    canEat (){
+        console.log("I can Eat")
+        return "I can Eat"
+    }
+}
+
+
+Alia["Father Name"] = "zbc"
+delete Alia["occupation"];
+console.log(Alia)
+
+
+
+
---
+++ Web-FSWD-Global-Age-Batch-no3/Javascript/Lecture no-2/readme.txt
@@ -0,0 +1,5 @@
+Continuation of Lecture no 2
+
+1 complex or derived Data types
+2 Funtions
+3 flow control and loops
