41e38501b98f6249ee3b114499c9fb97ac558636
fazi1live <faziislive@gmail.com>
2023-06-16 20:06:43 +0500

    add

---
+++ Web-FSWD-Global-Age-Batch-no3/Javascript/Lecture no-3/js/index.js
@@ -0,0 +1,18 @@
+
+
+// let result = function add(number1,number2){
+//     return number1+number2;
+// }
+
+let result = (ar1,arg2,arg3) =>  {
+}
+console.log(result(2,3,5))
+
+
+// number, ktinaytakKaroot
+
+// 2, 7
+
+// 2x2x2x2x2x2x2
+
+// console.log('hhihih'+undefined)
---
+++ Web-FSWD-Global-Age-Batch-no3/Javascript/Lecture no-3/readme.md
@@ -0,0 +1,4 @@
+Continution of Lecture no 2
+
+1 Funtions 
+2 Flow Controls
