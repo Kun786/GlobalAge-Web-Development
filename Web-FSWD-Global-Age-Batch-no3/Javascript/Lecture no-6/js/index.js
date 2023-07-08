cc853a5bafb1b13f5e5b916bf4f0d7b81a2e2df5
fazi1live <faziislive@gmail.com>
2023-07-08 21:11:19 +0500

    add

---
+++ Web-FSWD-Global-Age-Batch-no3/Javascript/Lecture no-6/js/index.js
@@ -0,0 +1,7 @@
+// 1 string concatenation
+
+let age = 18;
+
+// console.log("Hello My age is " +  age)
+
+console.log(`Hello World my age is ${age}`)
---
+++ Web-FSWD-Global-Age-Batch-no3/Javascript/Lecture no-6/readme.txt
@@ -0,0 +1,5 @@
+Today's lecture is about the.
+
+1 String Literals
+2 Switch Statement
+3 Loops
