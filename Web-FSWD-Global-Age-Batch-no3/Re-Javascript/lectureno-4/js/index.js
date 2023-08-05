2b4126a67dd81f02bfd7479fa347704f70faf326
fazi1live <faziislive@gmail.com>
2023-08-05 21:19:48 +0500

    add

---
+++ Web-FSWD-Global-Age-Batch-no3/Re-Javascript/lectureno-4/js/index.js
@@ -0,0 +1,35 @@
+let human = {
+    'my name': "John",
+    age: 30,
+    id:1,
+    location: "New York",
+    canEat(){
+        return true
+    }
+}
+
+const getId = human.id;
+
+// console.log(!!13);
+// console.log(!!-13);
+// console.log(!!0);
+// console.log(!!null);
+// console.log(!!undefined);
+// console.log(!!"asdas");
+// console.log(!!"");
+
+
+// Rules for Logic:
+// 1 Convert everyvalue to inherit value using !!
+// Always use strict comparison
+// use not opertraotr ! to chnage the boolean
+
+
+let x = 10;
+
+
+
+// Arthemetic operator  +,-,/,*,%
+//Logical operator  &&,|| !
+//Comparison Operators >,>=,<,<=,==,!=, ===
+
