b16f0a0f05ba84e7c0db78c47dae2e5055caad33
Kun786 <faziislive@gmail.com>
2024-02-18 18:40:46 +0500

    addd

--- Web-FSWD-Global-Age-Batch-no 5/lecture no - 19/index.js
+++ Web-FSWD-Global-Age-Batch-no 5/lecture no - 19/index.js
@@ -1,14 +1,27 @@
+
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
 const submitValues = () => {
     try {
         let getValue = document.querySelector('.input').value;
-        let getRadio = document.querySelector('.gender:checked').value
-        if(getValue !== "" && getRadio){
-            document.querySelector('h1').innerHTML = `Value is ${getValue} and color is`
-        }else{
-            alert('Please Enter All Details')
+        let getRadio = document.querySelector('.gender:checked').value;
+        let getEmail = document.querySelector('.email').value;
+
+        if (getValue !== "" && getRadio && validateEmail(getEmail)) {
+            document.querySelector('h1').innerHTML = `Value is ${getValue}, gender is ${getRadio}, and email is ${getEmail}`;
+        } else {
+            alert('Please Enter All Details or Correct Email');
         }
-        
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
+
+
+// for accessing data-value from html non-formcontrols
